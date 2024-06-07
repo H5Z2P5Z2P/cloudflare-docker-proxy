1debbf77f0f55dbea21efb2cf5d060c2dfb11692
H5Z2P5Z2P <sfly217@gmail.com>
2024-06-07 14:47:13 +0800

    my demo

--- src/index.js
+++ src/index.js
@@ -4,13 +4,13 @@ addEventListener("fetch", (event) => {
 });
 
 const routes = {
-  "docker.libcuda.so": "https://registry-1.docker.io",
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
+  "docker.epen.xyz": "https://registry-1.docker.io",
+  "quay.epen.xyz": "https://quay.io",
+  "gcr.epen.xyz": "https://gcr.io",
+  "k8s-gcr.epen.xyz": "https://k8s.gcr.io",
+  "k8s.epen.xyz": "https://registry.k8s.io",
+  "ghcr.epen.xyz": "https://ghcr.io",
+  "cloudsmith.epen.xyz": "https://docker.cloudsmith.io",
 };
 
 function routeByHosts(host) {
